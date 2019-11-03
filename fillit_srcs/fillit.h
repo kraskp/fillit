fee81823cea6250849f87ba306fd3d6e7795fa2b
Ken K <kraskp@uchicago.edu>
2019-11-03 16:43:56 +0200

    slight change to fillit.h

--- fillit_srcs/fillit.h
+++ fillit_srcs/fillit.h
@@ -6,18 +6,17 @@
 /*   By: kkraszew <kkraszew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/03 16:01:01 by kkraszew          #+#    #+#             */
-/*   Updated: 2019/11/03 16:33:31 by kkraszew         ###   ########.fr       */
+/*   Updated: 2019/11/03 16:43:33 by kkraszew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
-
+# define BUFF_SIZE 8
 # include <fcntl.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
-# define BUFF_SIZE 8
 
 typedef struct			s_info
 {
